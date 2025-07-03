4cd137fc0d634ebb9af71d58e6af14a2ecc6519d
Antoine-something <antoine.gaulin@mail.mcgill.ca>
2025-07-03 12:13:53 -0400

    Updated syntax to match the McTT file more

--- mechanization/theories/Core/Syntactic/System/Definitions.v
+++ mechanization/theories/Core/Syntactic/System/Definitions.v
@@ -1,17 +1,17 @@
 From Coq Require Import List Classes.RelationClasses Setoid Morphisms.
 
-From McPTS Require Import PtsSignature.
+From McPTS Require Import PtsSignature LibTactics.
 From McPTS.Core Require Import Base.
 From McPTS.Core.Syntactic Require Export Syntax.
 
 Reserved Notation "⊢ Γ" (in custom judg at level 80, Γ custom Exp).
-Reserved Notation "⊢ Γ ≡ Δ" (in custom judg at level 80, Γ custom Exp, Δ custom Exp).
+Reserved Notation "⊢ Γ ≈ Δ" (in custom judg at level 80, Γ custom Exp, Δ custom Exp).
 Reserved Notation "Γ ⊢ M : A" (in custom judg at level 80, Γ custom Exp, M custom Exp, A custom Exp).
-Reserved Notation "Γ ⊢ M ≡ N : A" (in custom judg at level 80, Γ custom Exp, M custom Exp, N custom Exp, A custom Exp).
+Reserved Notation "Γ ⊢ M ≈ N : A" (in custom judg at level 80, Γ custom Exp, M custom Exp, N custom Exp, A custom Exp).
 Reserved Notation "Γ ⊢ A" (in custom judg at level 80, Γ custom Exp, A custom Exp).
-Reserved Notation "Γ ⊢ A ≡ B" (in custom judg at level 80, Γ custom Exp, A custom Exp, B custom Exp).
+Reserved Notation "Γ ⊢ A ≈ B" (in custom judg at level 80, Γ custom Exp, A custom Exp, B custom Exp).
 Reserved Notation "Γ ⊢s σ : Δ" (in custom judg at level 80, Γ custom Exp, σ custom Exp, Δ custom Exp).
-Reserved Notation "Γ ⊢s σ ≡ τ : Δ" (in custom judg at level 80, Γ custom Exp, σ custom Exp, τ custom Exp, Δ custom Exp).
+Reserved Notation "Γ ⊢s σ ≈ τ : Δ" (in custom judg at level 80, Γ custom Exp, σ custom Exp, τ custom Exp, Δ custom Exp).
 Reserved Notation "'#' x : A :: K ∈ Γ" (in custom judg at level 80, x constr at level 0, A custom Exp, K custom Exp, Γ custom Exp).
 
 Generalizable All Variables.
@@ -31,7 +31,7 @@ Inductive wf_ctx {P : PtsSig} : Ctx P -> Prop :=
 where "⊢ Γ" := (wf_ctx Γ) (in custom judg) : type_scope
 
 (* Typing for expressions: Γ ⊢ M : A *)
-with wf_exp {P : PtsSig} : Ctx P -> Exp P -> Typ P -> Prop :=
+with wf_exp {P : PtsSig} : Ctx P -> Typ P -> Exp P -> Prop :=
 | wf_exp_st : `(Ax P s1 s2 -> {{ ⊢ Γ }} -> 
                 {{ Γ ⊢ Sort@s1 : Sort@s2 }})
 | wf_exp_var : `({{ ⊢ Γ }} -> {{ #i : A :: Sort@s ∈ Γ }} ->
@@ -47,9 +47,9 @@ with wf_exp {P : PtsSig} : Ctx P -> Exp P -> Typ P -> Prop :=
                      {{ Γ ⊢ M N : [Id,,N]B }})
 | wf_exp_clo : `({{ Γ ⊢s σ : Δ }} -> {{ Δ ⊢ M : A }} ->
                  {{ Γ ⊢ [σ]M : [σ]A }})
-| wf_exp_conv : `({{ Γ ⊢ M : A }} -> {{ Γ ⊢ A ≡ B }} ->
+| wf_exp_conv : `({{ Γ ⊢ M : A }} -> {{ Γ ⊢ A ≈ B }} ->
                   {{ Γ ⊢ M : B }})
-where "Γ ⊢ M : A" := (wf_exp Γ M A) (in custom judg) : type_scope
+where "Γ ⊢ M : A" := (wf_exp Γ A M) (in custom judg) : type_scope
 
 (* Type well-formedness: Γ ⊢ A *)
 with wf_typ {P : PtsSig} : Ctx P -> Typ P -> Prop :=
@@ -62,7 +62,7 @@ with wf_typ {P : PtsSig} : Ctx P -> Typ P -> Prop :=
 where "Γ ⊢ A" := (wf_typ Γ A) (in custom judg) : type_scope
 
 (* Typing for substitutions: Γ ⊢s σ : Δ *)
-with wf_sub {P : PtsSig} : Ctx P -> Sub P -> Ctx P -> Prop :=
+with wf_sub {P : PtsSig} : Ctx P -> Ctx P -> Sub P -> Prop :=
 | wf_sub_id : `({{ ⊢ Γ }} ->
                 {{ Γ ⊢s Id : Γ }})
 | wf_sub_wk : `({{ ⊢ Γ, A :: Sort@s }} ->
@@ -71,119 +71,232 @@ with wf_sub {P : PtsSig} : Ctx P -> Sub P -> Ctx P -> Prop :=
                  {{ Γ ⊢s σ,,M : Δ, A :: Sort@s }})
 | wf_sub_comp : `({{ Γ1 ⊢s σ : Γ2 }} -> {{ Γ2 ⊢s τ :Γ3 }} ->
                   {{ Γ1 ⊢s σ ∘ τ : Γ3 }})
-| wf_sub_conv : `({{ Γ ⊢s σ : Δ }} -> {{ ⊢ Δ ≡ Δ' }} ->
+| wf_sub_conv : `({{ Γ ⊢s σ : Δ }} -> {{ ⊢ Δ ≈ Δ' }} ->
                   {{ Γ ⊢s σ : Δ' }})
-where "Γ ⊢s σ : Δ" := (wf_sub Γ σ Δ) (in custom judg) : type_scope
+where "Γ ⊢s σ : Δ" := (wf_sub Γ Δ σ) (in custom judg) : type_scope
 
-(* Context equality: ⊢ Γ ≡ Δ *)
+(* Context equality: ⊢ Γ ≈ Δ *)
 with eq_ctx {P : PtsSig} : Ctx P -> Ctx P -> Prop :=
-| eq_ctx_nil : {{ ⊢ ⋅ ≡ ⋅ }}
-| eq_ctx_cons : `({{ ⊢ Γ ≡ Δ }} ->
+| eq_ctx_nil : {{ ⊢ ⋅ ≈ ⋅ }}
+| eq_ctx_cons : `({{ ⊢ Γ ≈ Δ }} ->
                   {{ Γ ⊢ A : Sort@s }} -> {{ Γ ⊢ B : Sort@s }} ->
                   {{ Δ ⊢ A : Sort@s }} -> {{ Δ ⊢ B : Sort@s }} ->
-                  {{ Γ ⊢ A ≡ B : Sort@s }} ->
-                  {{ ⊢ Γ, A :: Sort@s ≡ Δ, B :: Sort@s }})
-where "⊢ Γ ≡ Δ" := (eq_ctx Γ Δ) (in custom judg) : type_scope
+                  {{ Γ ⊢ A ≈ B : Sort@s }} -> {{ Δ ⊢ A ≈ B : Sort@s }} ->
+                  {{ ⊢ Γ, A :: Sort@s ≈ Δ, B :: Sort@s }})
+where "⊢ Γ ≈ Δ" := (eq_ctx Γ Δ) (in custom judg) : type_scope
 
-(* Expressions equality: Γ ⊢ M ≡ N : A *)
-with eq_exp {P : PtsSig} : Ctx P -> Exp P -> Exp P -> Typ P -> Prop :=
+(* Expressions equality: Γ ⊢ M ≈ N : A *)
+with eq_exp {P : PtsSig} : Ctx P -> Typ P -> Exp P -> Exp P -> Prop :=
 (* β-reduction and η-expansion *)
 | eq_exp_beta : `(forall r : Ru P s1 s2 s3,
                       {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ, A :: Sort@s1 ⊢ M : B }} -> {{ Γ ⊢ N : A }} ->
-                      {{ Γ ⊢ M N ≡ [Id,,N]M : [Id,,N]B }})
+                      {{ Γ ⊢ M N ≈ [Id,,N]M : [Id,,N]B }})
 | eq_exp_eta : `(forall r : Ru P s1 s2 s3,
                      {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ ⊢ M : Π r A B }} ->
-                     {{ Γ ⊢ M ≡ λ r ([Wk]M #0) : Π r A B }})
+                     {{ Γ ⊢ M ≈ λ r ([Wk]M #0) : Π r A B }})
 (* Substitution propagation *)
 | eq_exp_prop_sort : `(Ax P s1 s2 -> {{ Γ ⊢s σ : Δ }} ->
-                       {{ Γ ⊢ [σ]Sort@s1 ≡ Sort@s1 : Sort@s2 }})
+                       {{ Γ ⊢ [σ]Sort@s1 ≈ Sort@s1 : Sort@s2 }})
 | eq_exp_prop_var_ze : `({{ Γ ⊢s σ,,M : Δ, A :: Sort@s }} ->
-                         {{ Γ ⊢ [σ,,M]#0 ≡ M : [σ]A }})
+                         {{ Γ ⊢ [σ,,M]#0 ≈ M : [σ]A }})
 | eq_exp_prop_var_su : `({{ Γ ⊢s σ,,M : Δ, A :: Sort@s1 }} -> {{ #i : B :: Sort@s2 ∈ Δ }} ->
-                         {{ Γ ⊢ [σ,,M]#(S i) ≡ [σ]#i : [σ]B }})
+                         {{ Γ ⊢ [σ,,M]#(S i) ≈ [σ]#i : [σ]B }})
 | eq_exp_prop_pi : `(forall r : Ru P s1 s2 s3,
                          {{ Γ ⊢s σ : Δ }} -> {{ Δ ⊢ Π r A B : Sort@s3 }} ->
-                         {{ Γ ⊢ [σ](Π r A B) ≡ Π r ([σ] A) ([(Wk ∘ σ),,#0]B) : Sort@s3 }})
+                         {{ Γ ⊢ [σ](Π r A B) ≈ Π r ([σ] A) ([(Wk ∘ σ),,#0]B) : Sort@s3 }})
 | eq_exp_prop_lam : `(forall r : Ru P s1 s2 s3,
                           {{ Γ ⊢s σ : Δ }} -> {{ Δ ⊢ Π r A B : Sort@s3 }} -> {{ Δ, A :: Sort@s1 ⊢ M : B }} ->
-                          {{ Γ ⊢ [σ](λ r M) ≡ λ r ([(Wk∘σ),,#0]M) : [σ](Π r A B) }})
+                          {{ Γ ⊢ [σ](λ r M) ≈ λ r ([(Wk∘σ),,#0]M) : [σ](Π r A B) }})
 | eq_exp_prop_app : `(forall r : Ru P s1 s2 s3,
                           {{ Γ ⊢s σ : Δ }} -> {{ Δ ⊢ Π r A B : Sort@s3 }} -> {{ Δ ⊢ M : Π r A B }} -> {{ Δ ⊢ N : A }} ->
-                          {{ Γ ⊢ [σ](M N) ≡ [σ]M [σ]N : [σ,,[σ]N]B }})
+                          {{ Γ ⊢ [σ](M N) ≈ [σ]M [σ]N : [σ,,[σ]N]B }})
 | eq_exp_prop_id : `({{ Γ ⊢ M : A }} ->
-                     {{ Γ ⊢ [Id]M ≡ M : A }})
+                     {{ Γ ⊢ [Id]M ≈ M : A }})
 | eq_exp_prop_comp : `({{ Γ1 ⊢s σ1 : Γ2 }} -> {{ Γ2 ⊢s σ2 : Γ3 }} -> {{ Γ3 ⊢ M : A }} ->
-                       {{ Γ1 ⊢ [σ1 ∘ σ2]M ≡ [σ1][σ2]M : [σ1 ∘ σ2]A }})
+                       {{ Γ1 ⊢ [σ1 ∘ σ2]M ≈ [σ1][σ2]M : [σ1 ∘ σ2]A }})
 (* Congruence rules *)
 | eq_exp_cong_pi : `(forall r : Ru P s1 s2 s3,
-                         {{ Γ ⊢ A1 ≡ A2 : Sort@s1 }} -> {{ Γ, A1 :: Sort@s1 ⊢ B1 ≡ B2 : Sort@s2 }} ->
-                         {{ Γ ⊢ Π r A1 B1 ≡ Π r A2 B2 : Sort@s3 }})
+                         {{ Γ ⊢ A1 ≈ A2 : Sort@s1 }} -> {{ Γ, A1 :: Sort@s1 ⊢ B1 ≈ B2 : Sort@s2 }} ->
+                         {{ Γ ⊢ Π r A1 B1 ≈ Π r A2 B2 : Sort@s3 }})
 | eq_exp_cong_lam : `(forall r : Ru P s1 s2 s3,
-                          {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ, A :: Sort@s1 ⊢ M1 ≡ M2 : B }} ->
-                          {{ Γ ⊢ λM1 ≡ λM2 : Π r A B }})
+                          {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ, A :: Sort@s1 ⊢ M1 ≈ M2 : B }} ->
+                          {{ Γ ⊢ λM1 ≈ λM2 : Π r A B }})
 | eq_exp_cong_app : `(forall r : Ru P s1 s2 s3,
-                          {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ ⊢ M1 ≡ M2 : Π r A B }} -> {{ Γ ⊢ N1 ≡ N2 : A }} ->
-                          {{ Γ ⊢ M1 N1 ≡ M2 N2 : [Id,,N1]B }})
-| eq_exp_cong_clo : `({{ Γ ⊢s σ1 ≡ σ2 : Δ }} -> {{ Δ ⊢ M1 ≡ M2 : A }} ->
-                      {{ Γ ⊢ [σ1]M1 ≡ [σ2]M2 : [σ1]A }})
+                          {{ Γ ⊢ Π r A B : Sort@s3 }} -> {{ Γ ⊢ M1 ≈ M2 : Π r A B }} -> {{ Γ ⊢ N1 ≈ N2 : A }} ->
+                          {{ Γ ⊢ M1 N1 ≈ M2 N2 : [Id,,N1]B }})
+| eq_exp_cong_clo : `({{ Γ ⊢s σ1 ≈ σ2 : Δ }} -> {{ Δ ⊢ M1 ≈ M2 : A }} ->
+                      {{ Γ ⊢ [σ1]M1 ≈ [σ2]M2 : [σ1]A }})
 (* Equivalence relation rules *)
 | eq_exp_refl : `({{ Γ ⊢ M : A }} ->
-                  {{ Γ ⊢ M ≡ M : A }})
-| eq_exp_sym : `({{ Γ ⊢ M1 ≡ M2 : A }} ->
-                 {{ Γ ⊢ M2 ≡ M1 : A }})
-| eq_exp_trans : `({{ Γ ⊢ M1 ≡ M2 : A }} -> {{ Γ ⊢ M2 ≡ M3 : A }} ->
-                   {{ Γ ⊢ M1 ≡ M3 : A }})
+                  {{ Γ ⊢ M ≈ M : A }})
+| eq_exp_sym : `({{ Γ ⊢ M1 ≈ M2 : A }} ->
+                 {{ Γ ⊢ M2 ≈ M1 : A }})
+| eq_exp_trans : `({{ Γ ⊢ M1 ≈ M2 : A }} -> {{ Γ ⊢ M2 ≈ M3 : A }} ->
+                   {{ Γ ⊢ M1 ≈ M3 : A }})
 (* Conversion rule *)
-| eq_exp_conv : `({{ Γ ⊢ M1 ≡ M2 : A }} -> {{ Γ ⊢ A ≡ B }} ->
-                  {{ Γ ⊢ M1 ≡ M2 : B }})
-where "Γ ⊢ M ≡ N : A" := (eq_exp Γ M N A) (in custom judg) : type_scope
+| eq_exp_conv : `({{ Γ ⊢ M1 ≈ M2 : A }} -> {{ Γ ⊢ A ≈ B }} ->
+                  {{ Γ ⊢ M1 ≈ M2 : B }})
+where "Γ ⊢ M ≈ N : A" := (eq_exp Γ A M N) (in custom judg) : type_scope
 
-(* Type equality: Γ ⊢ A ≡ B *)
+(* Type equality: Γ ⊢ A ≈ B *)
 with eq_typ {P : PtsSig} : Ctx P -> Typ P -> Typ P -> Prop :=
 | eq_typ_st : `({{ ⊢ Γ }} ->
-                {{ Γ ⊢ Sort@s ≡ Sort@s }})
+                {{ Γ ⊢ Sort@s ≈ Sort@s }})
 | eq_typ_clo_st : `({{ Γ ⊢s σ : Δ }} ->
-                    {{ Γ ⊢ [σ]Sort@s ≡ Sort@s }})
-| eq_typ_exp : `({{ Γ ⊢ A ≡ B : Sort@s }} ->
-                 {{ Γ ⊢ A ≡ B }})
-| eq_typ_sym : `({{ Γ ⊢ A ≡ B }} ->
-                 {{ Γ ⊢ B ≡ B }})
-| eq_typ_trans : `({{ Γ ⊢ A1 ≡ A2 }} -> {{ Γ ⊢ A2 ≡ A3 }} ->
-                   {{ Γ ⊢ A1 ≡ A3 }})
-where "Γ ⊢ A ≡ B" := (eq_typ Γ A B) (in custom judg) : type_scope
+                    {{ Γ ⊢ [σ]Sort@s ≈ Sort@s }})
+| eq_typ_exp : `({{ Γ ⊢ A ≈ B : Sort@s }} ->
+                 {{ Γ ⊢ A ≈ B }})
+| eq_typ_sym : `({{ Γ ⊢ A ≈ B }} ->
+                 {{ Γ ⊢ B ≈ B }})
+| eq_typ_trans : `({{ Γ ⊢ A1 ≈ A2 }} -> {{ Γ ⊢ A2 ≈ A3 }} ->
+                   {{ Γ ⊢ A1 ≈ A3 }})
+where "Γ ⊢ A ≈ B" := (eq_typ Γ A B) (in custom judg) : type_scope
 
-(* Substitution equality: Γ ⊢s σ ≡ τ : Δ *)
-with eq_sub {P : PtsSig} : Ctx P -> Sub P -> Sub P -> Ctx P -> Prop :=
+(* Substitution equality: Γ ⊢s σ ≈ τ : Δ *)
+with eq_sub {P : PtsSig} : Ctx P -> Ctx P -> Sub P -> Sub P -> Prop :=
 (* β-reduction and η-expansion *)
-| eq_sub_beta_nil : `({{ ⋅ ⊢s Id ≡ .. : ⋅ }})
+| eq_sub_beta_nil : `({{ ⋅ ⊢s Id ≈ .. : ⋅ }})
 | eq_sub_beta_cons : `({{ ⊢ Γ, A :: Sort@s }} ->
-                       {{ Γ, A :: Sort@s ⊢s Id ≡ (Wk ∘ Id),,#0 : Γ, A :: Sort@s }})
+                       {{ Γ, A :: Sort@s ⊢s Id ≈ (Wk ∘ Id),,#0 : Γ, A :: Sort@s }})
 | eq_sub_eta : `({{ Γ ⊢s σ : Δ, A :: Sort@s }} ->
-                 {{ Γ ⊢s σ ≡ (σ ∘ Wk),,[σ]#0 : Δ, A :: Sort@s }})
+                 {{ Γ ⊢s σ ≈ (σ ∘ Wk),,[σ]#0 : Δ, A :: Sort@s }})
 (* Substitution propagation *)
 | eq_sub_prop_id_left : `({{ Γ ⊢s σ : Δ }} ->
-                          {{ Γ ⊢s Id ∘ σ ≡ σ : Δ }})
+                          {{ Γ ⊢s Id ∘ σ ≈ σ : Δ }})
 | eq_sub_prop_id_right : `({{ Γ ⊢s σ : Δ }} ->
-                           {{ Γ ⊢s σ ∘ Id ≡ σ : Δ }})
+                           {{ Γ ⊢s σ ∘ Id ≈ σ : Δ }})
 | eq_sub_prop_assoc : `({{ Γ1 ⊢s σ1 : Γ2 }} -> {{ Γ2 ⊢s σ2 : Γ3 }} -> {{ Γ3 ⊢s σ3 : Γ4 }} ->
-                        {{ Γ1 ⊢s (σ1 ∘ σ2) ∘ σ3 ≡ σ1 ∘ (σ2 ∘ σ3) : Γ4 }})
+                        {{ Γ1 ⊢s (σ1 ∘ σ2) ∘ σ3 ≈ σ1 ∘ (σ2 ∘ σ3) : Γ4 }})
 | eq_sub_prop_ext_left : `({{ Γ ⊢s σ : Δ }} -> {{ Δ ⊢ A }} -> {{ Γ ⊢ M : [σ]A }} ->
-                           {{ Γ ⊢s (σ,,M) ∘ Wk ≡ σ : Δ }})
+                           {{ Γ ⊢s (σ,,M) ∘ Wk ≈ σ : Δ }})
 | eq_sub_prop_ext_right : `({{ Γ1 ⊢s σ1 : Γ2 }} -> {{ Γ2 ⊢s σ2 : Γ3 }} -> {{ Γ3 ⊢ A : Sort@s }} -> {{ Γ2 ⊢ M : [σ2]A }} ->
-                            {{ Γ1 ⊢s σ1 ∘ (σ2,,M) ≡ (σ1 ∘ σ2),,[σ1]M : Γ3, A :: Sort@xs }})
+                            {{ Γ1 ⊢s σ1 ∘ (σ2,,M) ≈ (σ1 ∘ σ2),,[σ1]M : Γ3, A :: Sort@xs }})
 (* Congruence rules *)
-| eq_sub_cong_ext : `({{ Γ ⊢s σ1 ≡ σ2 : Δ }} -> {{ Δ ⊢ A : Sort@s }} -> {{ Γ ⊢ M1 ≡ M2 : [σ]A }} ->
-                      {{ Γ ⊢s σ1,,M1 ≡ σ2,,M2 : Δ, A :: Sort@s }})
-| eq_sub_cong_comp : `({{ Γ1 ⊢s σ1 ≡ σ2 : Γ2 }} -> {{ Γ2 ⊢s τ1 ≡ τ2 : Γ3 }} ->
-                       {{ Γ1 ⊢s σ1 ∘ τ1 ≡ σ2 ∘ τ2 : Γ3 }})
+| eq_sub_cong_ext : `({{ Γ ⊢s σ1 ≈ σ2 : Δ }} -> {{ Δ ⊢ A : Sort@s }} -> {{ Γ ⊢ M1 ≈ M2 : [σ]A }} ->
+                      {{ Γ ⊢s σ1,,M1 ≈ σ2,,M2 : Δ, A :: Sort@s }})
+| eq_sub_cong_comp : `({{ Γ1 ⊢s σ1 ≈ σ2 : Γ2 }} -> {{ Γ2 ⊢s τ1 ≈ τ2 : Γ3 }} ->
+                       {{ Γ1 ⊢s σ1 ∘ τ1 ≈ σ2 ∘ τ2 : Γ3 }})
 (* Equivalence relation rules *)
 | eq_sub_refl : `({{ Γ ⊢s σ : Δ }} ->
-                  {{ Γ ⊢s σ ≡ σ : Δ }})
-| eq_sub_sym : `({{ Γ ⊢s σ1 ≡ σ2 : Δ }} ->
-                 {{ Γ ⊢s σ2 ≡ σ1 : Δ }})
-| eq_sub_trans : `({{ Γ ⊢s σ1 ≡ σ2 : Δ }} -> {{ Γ ⊢s σ2 ≡ σ3 : Δ }} ->
-                   {{ Γ ⊢s σ1 ≡ σ3 : Δ }})
+                  {{ Γ ⊢s σ ≈ σ : Δ }})
+| eq_sub_sym : `({{ Γ ⊢s σ1 ≈ σ2 : Δ }} ->
+                 {{ Γ ⊢s σ2 ≈ σ1 : Δ }})
+| eq_sub_trans : `({{ Γ ⊢s σ1 ≈ σ2 : Δ }} -> {{ Γ ⊢s σ2 ≈ σ3 : Δ }} ->
+                   {{ Γ ⊢s σ1 ≈ σ3 : Δ }})
 (* Conversion rule *)
-| eq_sub_conv : `({{ Γ ⊢s σ1 ≡ σ2 : Δ1 }} -> {{ ⊢ Δ1 ≡ Δ2 }} ->
-                  {{ Γ ⊢s σ1 ≡ σ2 : Δ2 }})
-where "Γ ⊢s σ ≡ τ : Δ" := (eq_sub Γ σ τ Δ) (in custom judg) : type_scope.
+| eq_sub_conv : `({{ Γ ⊢s σ1 ≈ σ2 : Δ1 }} -> {{ ⊢ Δ1 ≈ Δ2 }} ->
+                  {{ Γ ⊢s σ1 ≈ σ2 : Δ2 }})
+where "Γ ⊢s σ ≈ τ : Δ" := (eq_sub Γ Δ σ τ) (in custom judg) : type_scope.
+
+
+Scheme wf_ctx_mut_ind := Induction for wf_ctx Sort Prop
+with wf_exp_mut_ind := Induction for wf_exp Sort Prop
+with wf_typ_mut_ind := Induction for wf_typ Sort Prop                                     
+with wf_sub_mut_ind := Induction for wf_sub Sort Prop
+with eq_ctx_mut_ind := Induction for eq_ctx Sort Prop
+with eq_exp_mut_ind := Induction for eq_exp Sort Prop
+with eq_typ_mut_ind := Induction for eq_typ Sort Prop
+with eq_sub_mut_ind := Induction for eq_sub Sort Prop.
+Combined Scheme syntactic_wf_mut_ind from
+  wf_ctx_mut_ind,
+  wf_exp_mut_ind,
+  wf_typ_mut_ind,
+  wf_sub_mut_ind,
+  eq_ctx_mut_ind,
+  eq_exp_mut_ind,
+  eq_typ_mut_ind,
+  eq_sub_mut_ind.
+
+
+Scheme wf_ctx_mut_ind' := Induction for wf_ctx Sort Prop
+with wf_exp_mut_ind' := Induction for wf_exp Sort Prop
+with wf_sub_mut_ind' := Induction for wf_sub Sort Prop.
+Combined Scheme syntactic_wf_mut_ind' from
+  wf_ctx_mut_ind',
+  wf_exp_mut_ind',
+  wf_sub_mut_ind'.
+
+
+#[export]
+Hint Constructors wf_ctx eq_ctx wf_exp eq_exp wf_typ eq_typ wf_sub eq_sub ctx_lookup : mctt.
+
+
+#[export]
+Instance eq_exp_PER {P : PtsSig} (Γ : Ctx P) A : PER (eq_exp Γ A).
+Proof.
+  split.
+  - eauto using eq_exp_sym.
+  - eauto using eq_exp_trans.
+Qed.
+
+#[export]
+Instance eq_sub_PER {P : PtsSig} (Γ Δ : Ctx P) : PER (eq_sub Γ Δ).
+Proof.
+  split.
+  - eauto using eq_sub_sym.
+  - eauto using eq_sub_trans.
+Qed.
+
+#[export]
+Instance eq_ctx_Symmetric {P : PtsSig} : Symmetric (@eq_ctx P).
+Proof.  
+  induction 1; econstructor; mauto; apply eq_exp_PER; mauto.
+Qed.
+
+
+Add Parametric Morphism {P : PtsSig} (Γ : Ctx P) T : (eq_exp Γ T)
+    with signature eq_exp Γ T ==> eq ==> iff as eq_exp_morphism_iff1.
+Proof.
+  split; mauto; intros; eapply eq_exp_trans; mauto.
+  eapply eq_exp_sym; mauto.
+Qed.
+
+
+
+Add Parametric Morphism {P : PtsSig} (Γ : Ctx P) T : (eq_exp Γ T)
+    with signature eq ==> eq_exp Γ T ==> iff as eq_exp_morphism_iff2.
+Proof.
+  split; mauto; intros; eapply eq_exp_trans; mauto.
+  eapply eq_exp_sym; mauto.
+Qed.
+
+Add Parametric Morphism {P : PtsSig} (Γ Δ : Ctx P) : (eq_sub Γ Δ)
+    with signature eq_sub Γ Δ ==> eq ==> iff as eq_sub_morphism_iff1.
+Proof.
+  split; mauto; intros; eapply eq_sub_trans; mauto.
+  eapply eq_sub_sym; mauto.
+Qed.
+
+Add Parametric Morphism {P : PtsSig} (Γ Δ : Ctx P) : (eq_sub Γ Δ)
+    with signature eq ==> eq_sub Γ Δ ==> iff as eq_sub_morphism_iff2.
+Proof.
+  split; mauto; intros; eapply eq_sub_trans; mauto.
+  eapply eq_sub_sym; mauto.
+Qed.
+
+
+(* There doesn't seem to be an alternative to using @ on every constructor *)
+(* Will this cause problem since now the PTS signature is assumed to be explicit? *)
+#[export]
+Hint Rewrite -> @eq_exp_prop_sort using mauto 3 : mcpts.
+
+#[export]
+Hint Rewrite -> @eq_sub_prop_id_right @eq_sub_prop_id_left
+                  @eq_sub_prop_assoc (* prefer right association *)
+                  @eq_sub_prop_ext_left using mauto 4 : mcpts.
+
+#[export]
+Hint Rewrite -> @eq_exp_prop_id @eq_exp_prop_pi using mauto 4 : mcpts.
+
+#[export]
+Instance wf_exp_eq_per_elem {P : PtsSig} (Γ : Ctx P) T : PERElem _ (wf_exp Γ T) (eq_exp Γ T).
+Proof.
+  intros a Ha; econstructor; mauto.
+Qed.
+
+
+#[export]
+Instance wf_sub_eq_per_elem {P : PtsSig} (Γ Δ : Ctx P) : PERElem _ (wf_sub Γ Δ) (eq_sub Γ Δ).
+Proof.
+  intros a Ha; econstructor; mauto.
+Qed.
--- mechanization/theories/Core/Syntactic/System/Lemmas.v
+++ mechanization/theories/Core/Syntactic/System/Lemmas.v
@@ -1,289 +1,95 @@
-From McPTS Require Import LibTactics.
+From McPTS Require Import PtsSignature LibTactics.
 From McPTS.Core Require Import Base.
-From McPTS.Core.PTSSignature Require Import Signature.
 From McPTS.Core.Syntactic.System Require Import Definitions.
 
-(* Basic properties of contexts *)
-Lemma ctx_lookup_lt {P : PtsSig} : forall {Γ : Ctx P} {x : nat} {A : Typ P} {s : St P},
-      {{ #x : A : s ∈ Γ }} ->
-      x < length Γ.
+(** ** Basic Context Properties *)
+
+Lemma ctx_lookup_lt {P : PtsSig} : forall {Γ : Ctx P} {A x s},
+    {{ #x : A :: Sort@s ∈ Γ }} ->
+    x < length Γ.
 Proof.
   induction 1; simpl; lia.
 Qed.
 #[export]
-  Hint Resolve ctx_lookup_lt : mcpts.
+Hint Resolve ctx_lookup_lt : mctt.
 
-
-Lemma functional_ctx_lookup {P : PtsSig} : forall {Γ : Ctx P} {x A A' s},
-    {{ #x : A : s ∈ Γ }} ->
-    {{ #x : A' : s ∈ Γ }} ->
+Lemma functional_ctx_lookup_typ {P : PtsSig} : forall {Γ : Ctx P} {A A' x s s'},
+    {{ #x : A :: Sort@s ∈ Γ }} ->
+    {{ #x : A' :: Sort@s' ∈ Γ }} ->
     A = A'.
 Proof with mautosolve.
-  intros * Hx Hx'; gen A'.
+  intros * Hx Hx'; gen s' A'.
   induction Hx as [|* ? IHHx]; intros; inversion_clear Hx';
     f_equal;
     intuition.
 Qed.
 
 
-Lemma ctx_decomp {P : PtsSig} : forall {Γ : Ctx P} {A s},
-    {{ ⊢ Γ, A : s}} -> {{ ⊢ Γ }} /\ exists i, {{ Γ ⊢ A : Sort@i }}.
+Lemma functional_ctx_lookup_st {P : PtsSig} : forall {Γ : Ctx P} {A A' x s s'},
+    {{ #x : A :: Sort@s ∈ Γ }} ->
+    {{ #x : A' :: Sort@s' ∈ Γ }} ->
+    s = s'.
+Proof with mautosolve.
+  intros * Hx Hx'; gen s' A'.
+  induction Hx as [|* ? IHHx]; intros; inversion_clear Hx'.
+  - admit.
+  - eapply IHHx; mauto.
+Admitted.
+
+
+Lemma functional_ctx_lookup {P : PtsSig} : forall {Γ : Ctx P} {A A' x s s'},
+    {{ #x : A :: Sort@s ∈ Γ }} ->
+    {{ #x : A' :: Sort@s' ∈ Γ }} ->
+    A = A' /\ s = s'.
+Proof.
+  intros; split.
+  - eapply functional_ctx_lookup_typ; mauto.
+  - eapply functional_ctx_lookup_st; mauto.
+Qed.
+
+
+
+Lemma ctx_decomp {P : PtsSig} : forall {Γ : Ctx P} {A s}, {{ ⊢ Γ, A :: Sort@s }} -> {{ ⊢ Γ }} /\ {{ Γ ⊢ A : Sort@s }}.
 Proof with now eauto.
   inversion 1...
 Qed.
 
 #[export]
-  Hint Resolve ctx_decomp : mcpts.
+Hint Resolve ctx_decomp : mctt.
 
-Corollary ctx_decomp_left {P : PtsSig} : forall {Γ : Ctx P} {A s}, {{ ⊢ Γ , A : s}} -> {{ ⊢ Γ }}.
+Corollary ctx_decomp_left {P : PtsSig} : forall {Γ : Ctx P} {A s}, {{ ⊢ Γ , A :: Sort@s }} -> {{ ⊢ Γ }}.
 Proof with easy.
   intros * ?%ctx_decomp...
 Qed.
 
-Corollary ctx_decomp_right {P : PtsSig} : forall {Γ : Ctx P} {A s}, {{ ⊢ Γ, A : s }} -> exists i, {{ Γ ⊢ A : Sort@i }}.
+Corollary ctx_decomp_right {P : PtsSig} : forall {Γ : Ctx P} {A s}, {{ ⊢ Γ, A :: Sort@s }} -> {{ Γ ⊢ A : Sort@s }}.
 Proof with easy.
   intros * ?%ctx_decomp...
 Qed.
 
 #[export]
-Hint Resolve ctx_decomp_left ctx_decomp_right : mcpts.
+Hint Resolve ctx_decomp_left ctx_decomp_right : mctt.
+
 
 
 (** ** Core Presuppositions *)
 
 (** *** Context Presuppositions *)
 
-Lemma presup_ctx_eq {P : PtsSig} : forall {Γ Δ : Ctx P}, {{ ⊢ Γ ≡ Δ }} -> {{ ⊢ Γ }} /\ {{ ⊢ Δ }}.
+Lemma presup_ctx_eq {P : PtsSig} : forall {Γ Δ : Ctx P}, {{ ⊢ Γ ≈ Δ }} -> {{ ⊢ Γ }} /\ {{ ⊢ Δ }}.
 Proof with mautosolve.
-  induction 1; split; econstructor; destruct_pairs; assumption.
+  induction 1; destruct_pairs...
 Qed.
 
-Corollary presup_ctx_eq_left {P : PtsSig} : forall {Γ Δ : Ctx P}, {{ ⊢ Γ ≡ Δ }} -> {{ ⊢ Γ }}.
+Corollary presup_ctx_eq_left : forall {Γ Δ}, {{ ⊢ Γ ≈ Δ }} -> {{ ⊢ Γ }}.
 Proof with easy.
   intros * ?%presup_ctx_eq...
 Qed.
 
-Corollary presup_ctx_eq_right {P : PtsSig} : forall {Γ Δ : Ctx P}, {{ ⊢ Γ ≡ Δ }} -> {{ ⊢ Δ }}.
+Corollary presup_ctx_eq_right : forall {Γ Δ}, {{ ⊢ Γ ≈ Δ }} -> {{ ⊢ Δ }}.
 Proof with easy.
   intros * ?%presup_ctx_eq...
 Qed.
 
 #[export]
-Hint Resolve presup_ctx_eq presup_ctx_eq_left presup_ctx_eq_right : mcpts.
-
-Lemma presup_sub {P : PtsSig}: forall {Γ Δ : Ctx P} {σ}, {{ Γ ⊢s σ : Δ }} -> {{ ⊢ Γ }} /\ {{ ⊢ Δ }}.
-Proof with mautosolve.
-  induction 1; split; mauto; destruct_pairs; mauto; econstructor; assumption.
-Qed.
-
-Corollary presup_sub_left {P : PtsSig} : forall {Γ Δ : Ctx P} {σ}, {{ Γ ⊢s σ : Δ }} -> {{ ⊢ Γ }}.
-Proof with easy.
-  intros * ?%presup_sub...
-Qed.
-
-Corollary presup_sub_right {P : PtsSig} : forall {Γ Δ : Ctx P} {σ}, {{ Γ ⊢s σ : Δ }} -> {{ ⊢ Δ }}.
-Proof with easy.
-  intros * ?%presup_sub...
-Qed.
-
-#[export]
-Hint Resolve presup_sub presup_sub_left presup_sub_right : mcpts.
-
-(** With [presup_sub], we can prove similar for [exp]. *)
-
-Lemma presup_exp_ctx {P : PtsSig} : forall {Γ : Ctx P} {M A}, {{ Γ ⊢ M : A }} -> {{ ⊢ Γ }}.
-Proof with mautosolve.
-  induction 1...
-Qed.
-
-#[export]
-Hint Resolve presup_exp_ctx : mcpts.
-
-(** and other presuppositions about context well-formedness. *)
-
-Lemma presup_sub_eq_ctx {P : PtsSig} : forall {Γ Δ : Ctx P} {σ σ'}, {{ Γ ⊢s σ ≡ σ' : Δ }} -> {{ ⊢ Γ }} /\ {{ ⊢ Δ }}.
-Proof with mautosolve.
-  induction 1; split; mauto; try econstructor; mauto; destruct_pairs; mauto.
-Qed.
-
-Corollary presup_sub_eq_ctx_left {P : PtsSig} : forall {Γ Δ : Ctx P} {σ σ'}, {{ Γ ⊢s σ ≡ σ' : Δ }} -> {{ ⊢ Γ }}.
-Proof with easy.
-  intros * ?%presup_sub_eq_ctx...
-Qed.
-
-Corollary presup_sub_eq_ctx_right {P : PtsSig} : forall {Γ Δ : Ctx P} {σ σ'}, {{ Γ ⊢s σ ≡ σ' : Δ }} -> {{ ⊢ Δ }}.
-Proof with easy.
-  intros * ?%presup_sub_eq_ctx...
-Qed.
-
-#[export]
-Hint Resolve presup_sub_eq_ctx presup_sub_eq_ctx_left presup_sub_eq_ctx_right : mcpts.
-
-Lemma presup_exp_eq_ctx {P : PtsSig} : forall {Γ : Ctx P} {M M' A}, {{ Γ ⊢ M ≡ M' : A }} -> {{ ⊢ Γ }}.
-Proof with mautosolve 2.
-  induction 1...
-Qed.
-
-#[export]
-Hint Resolve presup_exp_eq_ctx : mcpts.
-
-(** *** Immediate Results of Context Presuppositions *)
-
-Lemma wf_conv {P : PtsSig} : forall (Γ : Ctx P) M A i A',
-    {{ Γ ⊢ M : A }} ->
-    (** The next argument will be removed in SystemOpt *)
-    {{ Γ ⊢ A' : Sort@i }} ->
-    {{ Γ ⊢ A ≡ A' : Sort@i }} ->
-    {{ Γ ⊢ M : A' }}.
-Proof.
-  intros.
-  econstructor; mauto.
-  econstructor; mauto.  
-Qed.
-
-#[export]
-Hint Resolve wf_conv : mcpts.
-
-Lemma wf_sub_conv {P : PtsSig} : forall (Γ : Ctx P) σ Δ Δ',
-  {{ Γ ⊢s σ : Δ }} ->
-  {{ ⊢ Δ ≡ Δ' }} ->
-  {{ Γ ⊢s σ : Δ' }}.
-Proof.
-  intros.
-  econstructor; mauto.
-Qed.
-
-#[export]
-Hint Resolve wf_sub_conv : mcpts.
-
-Lemma wf_exp_eq_conv {P : PtsSig} : forall (Γ : Ctx P) M M' A A' i,
-   {{ Γ ⊢ M ≡ M' : A }} ->
-   (** The next argument will be removed in SystemOpt *)
-   {{ Γ ⊢ A' : Sort@i }} ->
-   {{ Γ ⊢ A ≡ A' : Sort@i }} ->
-   {{ Γ ⊢ M ≡ M' : A' }}.
-Proof. 
-  intros.
-  eapply eq_exp_conv; mauto.
-  econstructor; mauto.
-Qed.
-
-#[export]
-Hint Resolve wf_exp_eq_conv : mcpts.
-
-Lemma wf_sub_eq_conv {P : PtsSig} : forall (Γ : Ctx P) σ σ' Δ Δ',
-    {{ Γ ⊢s σ ≡ σ' : Δ }} ->
-    {{ ⊢ Δ ≡ Δ' }} ->
-    {{ Γ ⊢s σ ≡ σ' : Δ' }}.
-Proof.
-  intros.
-  eapply eq_sub_conv; mauto.
-Qed.
-
-#[export]
-Hint Resolve wf_sub_eq_conv : mcpts.
-
-
-(*  I don't understand how to port this to the PTS setting
-
-Add Parametric Morphism {P : PtsSig} (Γ : Ctx P) : (eq_sub Γ)
-    with signature eq_ctx ==> eq ==> eq ==> iff as wf_sub_eq_morphism_iff3.
-Proof.
-  intros Δ Δ' H **; split; [| symmetry in H]; mauto.
-Qed.
-*)
-
-
-Lemma ctx_eq_refl {P : PtsSig} : forall {Γ : Ctx P},
-    {{ ⊢ Γ }} ->
-    {{ ⊢ Γ ≡ Γ }}.
-Proof.
-  induction 1; econstructor; mauto 4.
-  econstructor; mauto.
-Qed.
-
-#[export]
-Hint Resolve ctx_eq_refl : mcpts.
-
-
-(** *** Lemmas for [exp] of [{{{ Type@i }}}] *)
-
-Lemma exp_sub_typ {P : PtsSig} : forall {Δ Γ : Ctx P} {A σ s},
-    {{ Δ ⊢ A : Sort@s }} ->
-    {{ Γ ⊢s σ : Δ }} ->
-    {{ Γ ⊢ [σ]A : Sort@s }}.
-Proof with mautosolve 3.
-  intros.
-  econstructor; mauto 3.
-  econstructor...
-  (* We are missing a rule here.
-   * The problem is that the rule for sorts does not apply, so there is no other choice but to find a s' such that (s : s') ∈ Ax, but that might not be the case
-   *)
-  admit.
-Admitted.
-
-#[export]
-Hint Resolve exp_sub_typ : mcpts.
-
-Lemma presup_ctx_lookup_typ {P : PtsSig} : forall {Γ : Ctx P} {A x s},
-    {{ ⊢ Γ }} ->
-    {{ #x : A : s ∈ Γ }} ->
-    {{ Γ ⊢ A : Sort@s }}.
-Proof with mautosolve 4.
-  intros * HΓ.  
-  induction 1; inversion_clear HΓ; eapply exp_sub_typ; mauto; econstructor; econstructor; assumption.
-Qed.
-
-#[export]
-Hint Resolve presup_ctx_lookup_typ : mcpts.
-
-
-Lemma exp_eq_sub_cong_typ1 {P : PtsSig} : forall {Δ Γ : Ctx P} {A A' σ s},
-    {{ Δ ⊢ A ≡ A' : Sort@s }} ->
-    {{ Γ ⊢s σ : Δ }} ->
-    {{ Γ ⊢ [σ]A ≡ [σ]A' : Sort@s }}.
-Proof with mautosolve 3.
-  intros.
-  (* Same problem as before: we cannot just apply conversion because it gives [σ]Sort@s *)
-Admitted.
-
-Lemma exp_eq_sub_cong_typ2' {P : PtsSig} : forall {Δ Γ : Ctx P} {A σ τ s},
-    {{ Δ ⊢ A : Sort@s }} ->
-    {{ Γ ⊢s σ : Δ }} ->
-    {{ Γ ⊢s σ ≡ τ : Δ }} ->
-    {{ Γ ⊢ [σ]A ≡ [τ]A : Sort@s }}.
-Proof with mautosolve 3.
-  (* Same problem as before *)
-Admitted.
-
-Lemma exp_eq_sub_compose_typ {P : PtsSig} : forall {Ψ Δ Γ : Ctx P} {A σ τ s},
-    {{ Ψ ⊢ A : Sort@s }} ->
-    {{ Δ ⊢s σ : Ψ }} ->
-    {{ Γ ⊢s τ : Δ }} ->
-    {{ Γ ⊢ [σ][τ]A ≡ [σ∘τ]A : Sort@s }}.
-Proof with mautosolve 3.
-  (* Same problem *)
-Admitted.
-
-#[export]
-Hint Resolve exp_eq_sub_cong_typ1 exp_eq_sub_cong_typ2' exp_eq_sub_compose_typ : mcpts.
-
-Lemma exp_eq_sub_compose_weaken_extend_typ {P : PtsSig} : forall {Γ : Ctx P} {σ Δ s1 A s2 B M},
-    {{ Γ ⊢s σ : Δ }} ->
-    {{ Δ ⊢ A : Sort@s1 }} ->
-    {{ Δ ⊢ B : Sort@s2 }} ->
-    {{ Γ ⊢ M : [σ]B }} ->
-    {{ Γ ⊢ [Wk][σ,,M]A ≡ [σ]A : Sort@s1 }}.
-Proof with mautosolve 3.
-  intros.
-  assert {{ Δ, B : s2 ⊢s Wk : Δ }}.
-  {
-    econstructor; mauto.
-    econstructor; mauto.
-  }
-  (* Same problem *)
-Admitted.
-
-#[export]
-Hint Resolve exp_eq_sub_compose_weaken_extend_typ : mcpts.
-
+Hint Resolve presup_ctx_eq presup_ctx_eq_left presup_ctx_eq_right : mctt.
