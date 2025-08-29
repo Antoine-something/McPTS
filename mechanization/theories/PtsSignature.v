fb5d65d4df6ff2dafc9b1490b8f2812f114d4ef6
Junyoung/Clare Jang <jjc9310@gmail.com>
2025-08-29 14:11:36 -0400

    Fix PER model

--- mechanization/theories/PtsSignature.v
+++ mechanization/theories/PtsSignature.v
@@ -13,9 +13,15 @@ Record PtsSig : Type :=
 Record PredicativeSig (P : PtsSig) : Type :=
   mkPredicativeSig {
       pred_rel : relation (St P);
-      pred_irrel : forall (s1 s2 : St P) (lt lt' : pred_rel s1 s2), lt = lt';
-      ord_rel : order (St P) pred_rel;
+      pred_irrel : forall {s1 s2 : St P} (lt lt' : pred_rel s1 s2), lt = lt';
+      ord_rel : order (St P) (fun s1 s2 => pred_rel s1 s2 \/ s1 = s2);
       wf_rel : well_founded pred_rel;
-      ord_ax : forall s1 s2 : St P, Ax P s1 s2 -> pred_rel s1 s2 /\ s1 <> s2;
-      ord_ru : forall s1 s2 s3 : St P, Ru P s1 s2 s3 -> (pred_rel s1 s3) /\ (pred_rel s2 s3);
+      ord_ax : forall {s1 s2 : St P}, Ax P s1 s2 -> pred_rel s1 s2;
+      ord_ru : forall {s1 s2 s3 : St P}, Ru P s1 s2 s3 -> (pred_rel s1 s3 \/ s1 = s3) /\ (pred_rel s2 s3 \/ s2 = s3);
     }.
+Arguments pred_rel {_}.
+Arguments pred_irrel {_} _ {_ _}.
+Arguments ord_rel {_}.
+Arguments wf_rel {_}.
+Arguments ord_ax {_} _ {_ _}.
+Arguments ord_ru {_} _ {_ _ _}.
