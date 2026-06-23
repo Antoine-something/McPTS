a8af3ee39a98fc4878e17cb25042aa292880aa27
Rafael Girardello <rafael.girardello@gmail.com>
2026-06-22 22:19:35 -0400

    up to subtyping

--- theories/Extraction/NbE.v
+++ theories/Extraction/NbE.v
@@ -1,33 +1,33 @@
 From Equations Require Import Equations.
 
-From Mctt Require Import LibTactics.
-From Mctt.Core Require Import Base.
-From Mctt.Core.Semantic Require Export NbE.
-From Mctt.Extraction Require Import Evaluation Readback.
+From McPTS Require Import PtsSignature LibTactics.
+From McPTS.Core Require Import Base.
+From McPTS.Core.Semantic Require Export NbE.
+From McPTS.Extraction Require Import Evaluation Readback.
 Import Domain_Notations.
 
 Generalizable All Variables.
 
-Inductive initial_env_order : ctx -> Prop :=
+Inductive initial_env_order {P : PtsSig} : (ctx P) -> Prop :=
 | ie_nil : initial_env_order nil
 | ie_cons :
   `( initial_env_order Γ ->
-     (forall p, initial_env Γ p ->
-           eval_exp_order A p) ->
-     initial_env_order (A :: Γ)).
+     (forall ρ, initial_env Γ ρ ->
+           eval_exp_order A ρ) ->
+     initial_env_order {{{ Γ, A@s }}}).
 
 #[local]
-Hint Constructors initial_env_order : mctt.
+Hint Constructors initial_env_order : mcpts.
 
-Lemma initial_env_order_sound : forall Γ p,
-    initial_env Γ p ->
+Lemma initial_env_order_sound {P : PtsSig} : forall (Γ : ctx P) ρ,
+    initial_env Γ ρ ->
     initial_env_order Γ.
 Proof with (econstructor; intros; functional_initial_env_rewrite_clear; functional_eval_rewrite_clear; mauto).
   induction 1...
 Qed.
 
 #[local]
-Hint Resolve initial_env_order_sound : mctt.
+Hint Resolve initial_env_order_sound : mcpts.
 
 Section InitialEnvImpl.
 
@@ -42,12 +42,12 @@ Section InitialEnvImpl.
     repeat impl_obl_tac1; try econstructor; mauto.
 
   #[tactic="impl_obl_tac",derive(equations=no,eliminator=no)]
-  Equations initial_env_impl G (H : initial_env_order G) : { p | initial_env G p } by struct H :=
+  Equations initial_env_impl {P : PtsSig} (Γ : ctx P) (H : initial_env_order Γ) : { ρ | initial_env Γ ρ } by struct H :=
   | nil, H => exist _ empty_env _
-  | cons A G, H =>
-      let (p, Hp) := initial_env_impl G _ in
-      let (a, Ha) := eval_exp_impl A p _ in
-      exist _ d{{{ p ↦ ⇑! a (length G) }}} _.
+  | {{{ Γ, A@s }}}, H =>
+      let (ρ, Hρ) := initial_env_impl Γ _ in
+      let (a, Ha) := eval_exp_impl A ρ _ in
+      exist _ d{{{ ρ ↦ ⇑! a (length Γ) }}} _.
 
 End InitialEnvImpl.
 
@@ -56,14 +56,14 @@ End InitialEnvImpl.
     is also obvious from the soundness of eval orders and functional
     nature of initial_env. *)
 
-Lemma initial_env_impl_complete : forall G p,
-    initial_env G p ->
-    exists H H', initial_env_impl G H = exist _ p H'.
+Lemma initial_env_impl_complete {P : PtsSig} : forall (Γ : ctx P) ρ,
+    initial_env Γ ρ ->
+    exists H H', initial_env_impl Γ H = exist _ ρ H'.
 Proof.
   intros.
-  assert (Horder : initial_env_order G) by mauto.
+  assert (Horder : initial_env_order Γ) by mauto.
   exists Horder.
-  destruct (initial_env_impl G Horder).
+  destruct (initial_env_impl Γ Horder).
   functional_initial_env_rewrite_clear.
   eexists; reflexivity.
 Qed.
@@ -86,26 +86,26 @@ Ltac functional_nbe_complete :=
       end
   end.
 
-Inductive nbe_order G M A : Prop :=
+Inductive nbe_order {P : PtsSig} (Γ : ctx P) M A : Prop :=
 | nbe_order_run :
-  `( initial_env_order G ->
-     (forall p, initial_env G p ->
-           eval_exp_order A p) ->
-     (forall p, initial_env G p ->
-           eval_exp_order M p) ->
-     (forall p a m,
-         initial_env G p ->
-         {{ ⟦ A ⟧ p ↘ a }} ->
-         {{ ⟦ M ⟧ p ↘ m }} ->
-         read_nf_order (length G) d{{{ ⇓ a m }}}) ->
-     nbe_order G M A ).
+  `( initial_env_order Γ ->
+     (forall ρ, initial_env Γ ρ ->
+           eval_exp_order A ρ) ->
+     (forall ρ, initial_env Γ ρ ->
+           eval_exp_order M ρ) ->
+     (forall ρ a m,
+         initial_env Γ ρ ->
+         {{ ⟦ A ⟧ ρ ↘ a }} ->
+         {{ ⟦ M ⟧ ρ ↘ m }} ->
+         read_nf_order (length Γ) d{{{ ⇓ a m }}}) ->
+     nbe_order Γ M A ).
 
 #[local]
-Hint Constructors nbe_order : mctt.
+Hint Constructors nbe_order : mcpts.
 
-Lemma nbe_order_sound : forall G M A w,
-    nbe G M A w ->
-    nbe_order G M A.
+Lemma nbe_order_sound {P : PtsSig} : forall (Γ : ctx P) M A w,
+    nbe Γ M A w ->
+    nbe_order Γ M A.
 Proof with (econstructor; intros;
             functional_initial_env_rewrite_clear;
             functional_eval_rewrite_clear;
@@ -115,7 +115,7 @@ Proof with (econstructor; intros;
 Qed.
 
 #[local]
-Hint Resolve nbe_order_sound : mctt.
+Hint Resolve nbe_order_sound : mcpts.
 
 Section NbEDef.
 
@@ -130,40 +130,40 @@ Section NbEDef.
     repeat impl_obl_tac1; try econstructor; mauto.
 
   #[tactic="impl_obl_tac",derive(equations=no,eliminator=no)]
-  Equations nbe_impl G M A (H : nbe_order G M A) : { w | nbe G M A w } by struct H :=
-  | G, M, A, H =>
-      let (p, Hp) := initial_env_impl G _ in
-      let (a, Ha) := eval_exp_impl A p _ in
-      let (m, Hm) := eval_exp_impl M p _ in
-      let (w, Hw) := read_nf_impl (length G) d{{{ ⇓ a m }}} _ in
+  Equations nbe_impl {P : PtsSig} (Γ : ctx P) M A (H : nbe_order Γ M A) : { w | nbe Γ M A w } by struct H :=
+  | Γ, M, A, H =>
+      let (ρ, Hρ) := initial_env_impl Γ _ in
+      let (a, Ha) := eval_exp_impl A ρ _ in
+      let (m, Hm) := eval_exp_impl M ρ _ in
+      let (w, Hw) := read_nf_impl (length Γ) d{{{ ⇓ a m }}} _ in
       exist _ w _.
 
 End NbEDef.
 
-Lemma nbe_impl_complete : forall G M A w,
-    nbe G M A w ->
-    exists H H', nbe_impl G M A H = exist _ w H'.
+Lemma nbe_impl_complete {P : PtsSig} : forall (Γ : ctx P) M A w,
+    nbe Γ M A w ->
+    exists H H', nbe_impl Γ M A H = exist _ w H'.
 Proof.
   intros; functional_nbe_complete.
 Qed.
 
-Inductive nbe_ty_order G A : Prop :=
+Inductive nbe_ty_order {P : PtsSig} (Γ : ctx P) A : Prop :=
 | nbe_ty_order_run :
-  `( initial_env_order G ->
-     (forall p, initial_env G p ->
-           eval_exp_order A p) ->
-     (forall p a,
-         initial_env G p ->
-         {{ ⟦ A ⟧ p ↘ a }} ->
-         read_typ_order (length G) a) ->
-     nbe_ty_order G A ).
+  `( initial_env_order Γ ->
+     (forall ρ, initial_env Γ ρ ->
+           eval_exp_order A ρ) ->
+     (forall ρ a,
+         initial_env Γ ρ ->
+         {{ ⟦ A ⟧ ρ ↘ a }} ->
+         read_typ_order (length Γ) a) ->
+     nbe_ty_order Γ A ).
 
 #[local]
-Hint Constructors nbe_ty_order : mctt.
+Hint Constructors nbe_ty_order : mcpts.
 
-Lemma nbe_ty_order_sound : forall G A w,
-    nbe_ty G A w ->
-    nbe_ty_order G A.
+Lemma nbe_ty_order_sound {P : PtsSig} : forall (Γ : ctx P) A w,
+    nbe_ty Γ A w ->
+    nbe_ty_order Γ A.
 Proof with (econstructor; intros;
             functional_initial_env_rewrite_clear;
             functional_eval_rewrite_clear;
@@ -173,7 +173,7 @@ Proof with (econstructor; intros;
 Qed.
 
 #[local]
-Hint Resolve nbe_ty_order_sound : mctt.
+Hint Resolve nbe_ty_order_sound : mcpts.
 
 Section NbETyDef.
 
@@ -188,18 +188,18 @@ Section NbETyDef.
     repeat impl_obl_tac1; try econstructor; mauto.
 
   #[tactic="impl_obl_tac",derive(equations=no,eliminator=no)]
-  Equations nbe_ty_impl G A (H : nbe_ty_order G A) : { w | nbe_ty G A w } by struct H :=
-  | G, A, H =>
-      let (p, Hp) := initial_env_impl G _ in
-      let (a, Ha) := eval_exp_impl A p _ in
-      let (w, Hw) := read_typ_impl (length G) a _ in
+  Equations nbe_ty_impl {P : PtsSig} (Γ : ctx P) A (H : nbe_ty_order Γ A) : { w | nbe_ty Γ A w } by struct H :=
+  | Γ, A, H =>
+      let (ρ, Hρ) := initial_env_impl Γ _ in
+      let (a, Ha) := eval_exp_impl A ρ _ in
+      let (w, Hw) := read_typ_impl (length Γ) a _ in
       exist _ w _.
 
 End NbETyDef.
 
-Lemma nbe_ty_impl_complete : forall G A w,
-    nbe_ty G A w ->
-    exists H H', nbe_ty_impl G A H = exist _ w H'.
+Lemma nbe_ty_impl_complete {P : PtsSig} : forall (Γ : ctx P) A w,
+    nbe_ty Γ A w ->
+    exists H H', nbe_ty_impl Γ A H = exist _ w H'.
 Proof.
   intros; functional_nbe_complete.
 Qed.
--- theories/Extraction/Readback.v
+++ theories/Extraction/Readback.v
@@ -1,155 +1,99 @@
 From Equations Require Import Equations.
 
-From Mctt Require Import LibTactics.
-From Mctt.Core Require Import Base.
-From Mctt.Core.Semantic Require Import Readback Evaluation.
-From Mctt.Extraction Require Import Evaluation.
+From McPTS Require Import PtsSignature LibTactics.
+From McPTS.Core Require Import Base.
+From McPTS.Core.Semantic Require Import Readback Evaluation.
+From McPTS.Extraction Require Import Evaluation.
 Import Domain_Notations.
 
 Generalizable All Variables.
 
-Inductive read_nf_order : nat -> domain_nf -> Prop :=
+Inductive read_nf_order {P : PtsSig} : nat -> domain_nf P -> Prop :=
 | rnf_type :
-  `( read_typ_order s a ->
-    read_nf_order s d{{{ ⇓ 𝕌@i a }}} )
-| rnf_zero :
-  `( read_nf_order s d{{{ ⇓ ℕ zero }}} )
-| rnf_succ :
-  `( read_nf_order s d{{{ ⇓ ℕ m }}} ->
-     read_nf_order s d{{{ ⇓ ℕ (succ m) }}} )
-| rnf_nat_neut :
-  `( read_ne_order s m ->
-     read_nf_order s d{{{ ⇓ ℕ (⇑ a m) }}} )
+  `( read_typ_order i a ->
+    read_nf_order i d{{{ ⇓ Sort@s a }}} )
 | rnf_fn :
-  `( read_typ_order s a ->
-     eval_app_order m d{{{ ⇑! a s }}} ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s }}} ->
-     (forall m' b,
-         {{ $| m & ⇑! a s |↘ m' }} ->
-         {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
-         read_nf_order (S s) d{{{ ⇓ b m' }}}) ->
-     read_nf_order s d{{{ ⇓ (Π a p B) m }}} )
-| rnf_pair :
-  `( read_typ_order s a ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s }}} ->
-     eval_fst_order m ->
-     eval_snd_order m ->
-     (forall b,
-          {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
-          read_typ_order (S s) b) ->
-     (forall m1,
-         {{ π₁ m ↘ m1 }} ->
-         eval_exp_order B d{{{ p ↦ m1 }}} ) ->
-     (forall m1,
-         {{ π₁ m ↘ m1 }} ->
-         read_nf_order s d{{{ ⇓ a m1 }}}) ->
-     (forall m1 m2 b,
-         {{ π₁ m ↘ m1 }} ->
-         {{ π₂ m ↘ m2 }} ->
-         {{ ⟦ B ⟧ p ↦ m1 ↘ b }} ->
-         read_nf_order s d{{{ ⇓ b m2 }}}) ->
-     read_nf_order s d{{{ ⇓ (Σ a p B) m }}} )
-| rnf_refl :
-  `( read_typ_order s a ->
-     read_nf_order s d{{{ ⇓ a m' }}} ->
-     read_nf_order s d{{{ ⇓ (Eq a m1 m2) (refl m') }}} )
-| rnf_eq_neut :
-  `( read_ne_order s n ->
-     read_nf_order s d{{{ ⇓ (Eq a m1 m2) (⇑ b n) }}} )
+  `( forall (r : Ru_pi P s1 s2 s3),
+        read_typ_order i a ->
+        eval_app_order m d{{{ ⇑! a i }}} ->
+        eval_exp_order B d{{{ p ↦ ⇑! a i }}} ->
+        (forall b,
+            {{ ⟦ B ⟧ p ↦ ⇑! a i ↘ b }} ->
+            read_typ_order (S i) b) ->
+        (forall m' b,
+            {{ $| m & ⇑! a i |↘ m' }} ->
+            {{ ⟦ B ⟧ p ↦ ⇑! a i ↘ b }} ->
+            read_nf_order (S i) d{{{ ⇓ b m' }}}) ->
+        read_nf_order i d{{{ ⇓ (Π r a p B) m }}} )
+| rnf_zero :
+  `( read_nf_order i d{{{ ⇓ ℕ zero }}} )
+| rnf_succ :
+  `( read_nf_order i d{{{ ⇓ ℕ m }}} ->
+     read_nf_order i d{{{ ⇓ ℕ (succ m) }}} )
+| rnf_nat_neut :
+  `( read_ne_order i m ->
+     read_nf_order i d{{{ ⇓ ℕ (⇑ a m) }}} )
 | rnf_neut :
-  `( read_ne_order s m ->
-     read_nf_order s d{{{ ⇓ (⇑ a b) (⇑ c m) }}} )
+  `( read_ne_order i m ->
+     read_nf_order i d{{{ ⇓ (⇑ a b) (⇑ c m) }}} )
 
-with read_ne_order : nat -> domain_ne -> Prop :=
+with read_ne_order {P : PtsSig} : nat -> domain_ne P -> Prop :=
 | rne_var :
-  `( read_ne_order s d{{{ !x }}} )
+  `( read_ne_order i d{{{ !x }}} )
 | rne_app :
-  `( read_ne_order s m ->
-     read_nf_order s n ->
-     read_ne_order s d{{{ m n }}} )
-| rne_fst :
-  `( read_ne_order s m ->
-     read_ne_order s d{{{ fst m }}} )
-| rne_snd :
-  `( read_ne_order s m ->
-     read_ne_order s d{{{ snd m }}} )
+  `( read_ne_order i m ->
+     read_nf_order i n ->
+     read_ne_order i d{{{ m n }}} )
 | rne_natrec :
-  `( eval_exp_order B d{{{ p ↦ ⇑! ℕ s }}} ->
+  `( eval_exp_order B d{{{ p ↦ ⇑! ℕ i }}} ->
      (forall b,
-         {{ ⟦ B ⟧ p ↦ ⇑! ℕ s ↘ b }} ->
-         read_typ_order (S s) b) ->
+         {{ ⟦ B ⟧ p ↦ ⇑! ℕ i ↘ b }} ->
+         read_typ_order (S i) b) ->
      eval_exp_order B d{{{ p ↦ zero }}} ->
      (forall bz,
          {{ ⟦ B ⟧ p ↦ zero ↘ bz }} ->
-         read_nf_order s d{{{ ⇓ bz mz }}}) ->
-     eval_exp_order B d{{{ p ↦ succ (⇑! ℕ s) }}} ->
+         read_nf_order i d{{{ ⇓ bz mz }}}) ->
+     eval_exp_order B d{{{ p ↦ succ (⇑! ℕ i) }}} ->
      (forall b,
-         {{ ⟦ B ⟧ p ↦ ⇑! ℕ s ↘ b }} ->
-         eval_exp_order MS d{{{ p ↦ ⇑! ℕ s ↦ ⇑! b (S s) }}}) ->
+         {{ ⟦ B ⟧ p ↦ ⇑! ℕ i ↘ b }} ->
+         eval_exp_order MS d{{{ (p ↦ ⇑! ℕ i) ↦ ⇑! b (S i) }}}) ->
      (forall b bs ms,
-         {{ ⟦ B ⟧ p ↦ ⇑! ℕ s ↘ b }} ->
-         {{ ⟦ B ⟧ p ↦ succ (⇑! ℕ s) ↘ bs }} ->
-         {{ ⟦ MS ⟧ p ↦ ⇑! ℕ s ↦ ⇑! b (S s) ↘ ms }} ->
-         read_nf_order (S (S s)) d{{{ ⇓ bs ms }}}) ->
-     read_ne_order s m ->
-     read_ne_order s d{{{ rec m under p return B | zero -> mz | succ -> MS end }}} )
-| read_ne_eqrec :
-  `( read_typ_order s a ->
-     read_nf_order s d{{{ ⇓ a m1 }}} ->
-     read_nf_order s d{{{ ⇓ a m2 }}} ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s ↦ ⇑! a (S s) ↦ ⇑! (Eq a (⇑! a s) (⇑! a (S s))) (S (S s)) }}} ->
-     (forall b,
-         {{ ⟦ B ⟧ p ↦ ⇑! a s ↦ ⇑! a (S s) ↦ ⇑! (Eq a (⇑! a s) (⇑! a (S s))) (S (S s)) ↘ b }} ->
-         read_typ_order (S (S (S s))) b) ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s ↦ ⇑! a s ↦ refl (⇑! a s) }}} ->
-     eval_exp_order BR d{{{ p ↦ ⇑! a s }}} ->
-     (forall b br,
-         {{ ⟦ B ⟧ p ↦ ⇑! a s ↦ ⇑! a s ↦ refl (⇑! a s) ↘ b }} ->
-         {{ ⟦ BR ⟧ p ↦ ⇑! a s ↘ br }} ->
-         read_nf_order (S s) d{{{ ⇓ b br }}}) ->
-     read_ne_order s n ->
-     read_ne_order s d{{{ eqrec n under p as Eq a m1 m2 return B | refl -> BR end }}} )
+         {{ ⟦ B ⟧ p ↦ ⇑! ℕ i ↘ b }} ->
+         {{ ⟦ B ⟧ p ↦ succ (⇑! ℕ i) ↘ bs }} ->
+         {{ ⟦ MS ⟧ (p ↦ ⇑! ℕ i) ↦ ⇑! b (S i) ↘ ms }} ->
+         read_nf_order (S (S i)) d{{{ ⇓ bs ms }}}) ->
+     read_ne_order i m ->
+     read_ne_order i d{{{ rec m under p return B | zero -> mz | succ -> MS end }}} )
 
-with read_typ_order : nat -> domain -> Prop :=
-| rtyp_univ :
-  `( read_typ_order s d{{{ 𝕌@i }}} )
+with read_typ_order {P : PtsSig} : nat -> domain P -> Prop :=
+| rtyp_sort :
+  `( read_typ_order i d{{{ Sort@s }}} )
 | rtyp_nat :
-  `( read_typ_order s d{{{ ℕ }}} )
+  `( read_typ_order i d{{{ ℕ }}} )
 | rtyp_pi :
-  `( read_typ_order s a ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s }}} ->
+  `( forall (r : Ru_pi P s1 s2 s3),
+      read_typ_order i a ->
+     eval_exp_order B d{{{ p ↦ ⇑! a i }}} ->
      (forall b,
-         {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
-         read_typ_order (S s) b) ->
-     read_typ_order s d{{{ Π a p B }}})
-| rtyp_sigma :
-  `( read_typ_order s a ->
-     eval_exp_order B d{{{ p ↦ ⇑! a s }}} ->
-     (forall b,
-         {{ ⟦ B ⟧ p ↦ ⇑! a s ↘ b }} ->
-         read_typ_order (S s) b) ->
-     read_typ_order s d{{{ Σ a p B }}})
-| rtyp_eq :
-  `( read_typ_order s a ->
-     read_nf_order s d{{{ ⇓ a m1 }}} ->
-     read_nf_order s d{{{ ⇓ a m2 }}} ->
-     read_typ_order s d{{{ Eq a m1 m2 }}})
+         {{ ⟦ B ⟧ p ↦ ⇑! a i ↘ b }} ->
+         read_typ_order (S i) b) ->
+     read_typ_order i d{{{ Π r a p B }}})
 | rtyp_neut :
-  `( read_ne_order s b ->
-     read_typ_order s d{{{ ⇑ a b }}} ).
+  `( read_ne_order i b ->
+     read_typ_order i d{{{ ⇑ a b }}} ).
 
 #[local]
-Hint Constructors read_nf_order read_ne_order read_typ_order : mctt.
+Hint Constructors read_nf_order read_ne_order read_typ_order : mcpts.
 
-Lemma read_nf_order_sound : forall s d m,
-    {{ Rnf d in s ↘ m }} ->
-    read_nf_order s d
-with read_ne_order_sound : forall s d m,
-    {{ Rne d in s ↘ m }} ->
-    read_ne_order s d
-with read_typ_order_sound : forall s d m,
-    {{ Rtyp d in s ↘ m }} ->
-    read_typ_order s d.
+Lemma read_nf_order_sound {P : PtsSig} : forall i (d : domain_nf P) m,
+    {{ Rnf d in i ↘ m }} ->
+    read_nf_order i d
+with read_ne_order_sound {P : PtsSig} : forall i (d : domain_ne P) m,
+    {{ Rne d in i ↘ m }} ->
+    read_ne_order i d
+with read_typ_order_sound {P : PtsSig} : forall i (d : domain P) m,
+    {{ Rtyp d in i ↘ m }} ->
+    read_typ_order i d.
 Proof with (econstructor; intros; functional_eval_rewrite_clear; mauto).
   - clear read_nf_order_sound; induction 1...
   - clear read_ne_order_sound; induction 1...
@@ -157,7 +101,7 @@ Proof with (econstructor; intros; functional_eval_rewrite_clear; mauto).
 Qed.
 
 #[export]
-Hint Resolve read_nf_order_sound read_ne_order_sound read_typ_order_sound : mctt.
+Hint Resolve read_nf_order_sound read_ne_order_sound read_typ_order_sound : mcpts.
 
 #[local]
 Ltac impl_obl_tac1 :=
@@ -172,98 +116,55 @@ Ltac impl_obl_tac :=
   repeat impl_obl_tac1; try econstructor; mauto.
 
 #[tactic="impl_obl_tac",derive(equations=no,eliminator=no)]
-Equations read_nf_impl s d (H : read_nf_order s d) : { m | {{ Rnf d in s ↘ m }} } by struct H :=
-| s, d{{{ ⇓ 𝕌@i a }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
+Equations read_nf_impl {P : PtsSig} i (d : domain_nf P) (H : read_nf_order i d) : { m | {{ Rnf d in i ↘ m }} } by struct H :=
+| i, d{{{ ⇓ Sort@s a }}}, H =>
+    let (A, HA) := read_typ_impl i a _ in
     exist _ A _
-| s, d{{{ ⇓ ℕ zero }}}    , H => exist _ n{{{ zero }}} _
-| s, d{{{ ⇓ ℕ (succ m) }}}, H =>
-    let (M, HM) := read_nf_impl s d{{{ ⇓ ℕ m }}} _ in
+| i, d{{{ ⇓ (Π r a p B) m }}}, H =>
+    let (A, HA) := read_typ_impl i a _ in
+    let (m', Hm') := eval_app_impl m d{{{ ⇑! a i }}} _ in
+    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a i }}} _ in
+    let (B', HB') := read_typ_impl (S i) b _ in
+    let (M, HM) := read_nf_impl (S i) d{{{ ⇓ b m' }}} _ in
+    exist _ n{{{ λ r A B' M }}} _
+| i, d{{{ ⇓ ℕ zero }}}    , H => exist _ n{{{ zero }}} _
+| i, d{{{ ⇓ ℕ (succ m) }}}, H =>
+    let (M, HM) := read_nf_impl i d{{{ ⇓ ℕ m }}} _ in
     exist _ n{{{ succ M }}} _
-| s, d{{{ ⇓ ℕ (⇑ ^_ m) }}}, H =>
-    let (M, HM) := read_ne_impl s m _ in
+| i, d{{{ ⇓ ℕ (⇑ ^_ m) }}}, H =>
+    let (M, HM) := read_ne_impl i m _ in
     exist _ n{{{ ⇑ M }}} _
-| s, d{{{ ⇓ (Π a p B) m }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (m', Hm') := eval_app_impl m d{{{ ⇑! a s }}} _ in
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a s }}} _ in
-    let (M, HM) := read_nf_impl (S s) d{{{ ⇓ b m' }}} _ in
-    exist _ n{{{ λ A M }}} _
-| s, d{{{ ⇓ (Σ a p B) m }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a s }}} _ in
-    let (B', HB) := read_typ_impl (S s) b _ in
-    let (m1, Hm1) := eval_fst_impl m _ in
-    let (m2, Hm2) := eval_snd_impl m _ in
-    let (M, HM) := read_nf_impl s d{{{ ⇓ a m1 }}} _ in
-    let (b', Hb') := eval_exp_impl B d{{{ p ↦ m1 }}} _ in
-    let (N, HN) := read_nf_impl s d{{{ ⇓ b' m2 }}} _ in
-    exist _ n{{{ ⟨ M : A ; N : B' ⟩ }}} _
-| s, d{{{ ⇓ (Eq a m1 m2) (refl m') }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (M', HM') := read_nf_impl s d{{{ ⇓ a m' }}} _ in
-    exist _ n{{{ refl A M' }}} _
-| s, d{{{ ⇓ (Eq a m1 m2) (⇑ b n) }}}, H =>
-    let (N, HN) := read_ne_impl s n _ in
-    exist _ n{{{ ⇑ N }}} _
-| s, d{{{ ⇓ (⇑ a b) (⇑ c m) }}}, H =>
-    let (M, HM) := read_ne_impl s m _ in
+| i, d{{{ ⇓ (⇑ a b) (⇑ c m) }}}, H =>
+    let (M, HM) := read_ne_impl i m _ in
     exist _ n{{{ ⇑ M }}} _
 
-with read_ne_impl s d (H : read_ne_order s d) : { m | {{ Rne d in s ↘ m }} } by struct H :=
-| s, d{{{ !x }}}, H => exist _ n{{{ #(s - x - 1) }}} _
-| s, d{{{ m n }}}, H =>
-    let (M, HM) := read_ne_impl s m _ in
-    let (N, HN) := read_nf_impl s n _ in
+with read_ne_impl {P : PtsSig} i (d : domain_ne P) (H : read_ne_order i d) : { m | {{ Rne d in i ↘ m }} } by struct H :=
+| i, d{{{ !x }}}, H => exist _ n{{{ #(i - x - 1) }}} _
+| i, d{{{ m n }}}, H =>
+    let (M, HM) := read_ne_impl i m _ in
+    let (N, HN) := read_nf_impl i n _ in
     exist _ n{{{ M N }}} _
-| s, d{{{ fst m }}}, H =>
-    let (M, HM) := read_ne_impl s m _ in
-    exist _ n{{{ fst M }}} _
-| s, d{{{ snd m }}}, H =>
-    let (M, HM) := read_ne_impl s m _ in
-    exist _ n{{{ snd M }}} _
-| s, d{{{ rec m under p return B | zero -> mz | succ -> MS end }}}, H =>
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! ℕ s }}} _ in
-    let (B', HB') := read_typ_impl (S s) b _ in
+| i, d{{{ rec m under p return B | zero -> mz | succ -> MS end }}}, H =>
+    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! ℕ i }}} _ in
+    let (B', HB') := read_typ_impl (S i) b _ in
     let (bz, Hbz) := eval_exp_impl B d{{{ p ↦ zero }}} _ in
-    let (MZ, HMZ) := read_nf_impl s d{{{ ⇓ bz mz }}} _ in
-    let (bs, Hbs) := eval_exp_impl B d{{{ p ↦ succ (⇑! ℕ s) }}} _ in
-    let (ms, Hms) := eval_exp_impl MS d{{{ p ↦ ⇑! ℕ s ↦ ⇑! b (S s) }}} _ in
-    let (MS', HMS') := read_nf_impl (S (S s)) d{{{ ⇓ bs ms }}} _ in
-    let (M, HM) := read_ne_impl s m _ in
+    let (MZ, HMZ) := read_nf_impl i d{{{ ⇓ bz mz }}} _ in
+    let (bs, Hbs) := eval_exp_impl B d{{{ p ↦ succ (⇑! ℕ i) }}} _ in
+    let (ms, Hms) := eval_exp_impl MS d{{{ (p ↦ ⇑! ℕ i) ↦ ⇑! b (S i) }}} _ in
+    let (MS', HMS') := read_nf_impl (S (S i)) d{{{ ⇓ bs ms }}} _ in
+    let (M, HM) := read_ne_impl i m _ in
     exist _ n{{{ rec M return B' | zero -> MZ | succ -> MS' end }}} _
-| s, d{{{ eqrec n under p as Eq a m1 m2 return B | refl -> BR end }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (M1, HM1) := read_nf_impl s d{{{ ⇓ a m1 }}} _ in
-    let (M2, HM2) := read_nf_impl s d{{{ ⇓ a m2 }}} _ in
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a s ↦ ⇑! a (S s) ↦ ⇑! (Eq a (⇑! a s) (⇑! a (S s))) (S (S s)) }}} _ in
-    let (B', HB') := read_typ_impl (S (S (S s))) b _ in
-    let (bbr, Hbbr) := eval_exp_impl B d{{{ p ↦ ⇑! a s ↦ ⇑! a s ↦ refl (⇑! a s) }}} _ in
-    let (br, Hbr) := eval_exp_impl BR d{{{ p ↦ ⇑! a s }}} _ in
-    let (BR', HBR') := read_nf_impl (S s) d{{{ ⇓ bbr br }}} _ in
-    let (N, HN) := read_ne_impl s n _ in
-    exist _ n{{{ eqrec N as Eq A M1 M2 return B' | refl -> BR' end }}} _
 
-with read_typ_impl s d (H : read_typ_order s d) : { m | {{ Rtyp d in s ↘ m }} } by struct H :=
-| s, d{{{ 𝕌@i }}}, H => exist _ n{{{ Type@i }}} _
-| s, d{{{ ℕ }}}, H => exist _ n{{{ ℕ }}} _
-| s, d{{{ Π a p B }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a s }}} _ in
-    let (B', HB') := read_typ_impl (S s) b _ in
-    exist _ n{{{ Π A B' }}} _
-| s, d{{{ Σ a p B }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a s }}} _ in
-    let (B', HB') := read_typ_impl (S s) b _ in
-    exist _ n{{{ Σ A B' }}} _
-| s, d{{{ Eq a m1 m2 }}}, H =>
-    let (A, HA) := read_typ_impl s a _ in
-    let (M1, HM1) := read_nf_impl s d{{{ ⇓ a m1 }}} _ in
-    let (M2, HM2) := read_nf_impl s d{{{ ⇓ a m2 }}} _ in
-    exist _ n{{{ Eq A M1 M2 }}} _
-| s, d{{{ ⇑ a b }}}, H =>
-    let (B, HB) := read_ne_impl s b _ in
+with read_typ_impl {P : PtsSig} i (d : domain P) (H : read_typ_order i d) : { m | {{ Rtyp d in i ↘ m }} } by struct H :=
+| i, d{{{ Sort@s }}}, H => exist _ n{{{ Sort@s }}} _
+| i, d{{{ ℕ }}}, H => exist _ n{{{ ℕ }}} _
+| i, d{{{ Π r a p B }}}, H =>
+    let (A, HA) := read_typ_impl i a _ in
+    let (b, Hb) := eval_exp_impl B d{{{ p ↦ ⇑! a i }}} _ in
+    let (B', HB') := read_typ_impl (S i) b _ in
+    exist _ n{{{ Π r A B' }}} _
+| i, d{{{ ⇑ a b }}}, H =>
+    let (B, HB) := read_ne_impl i b _ in
     exist _ n{{{ ⇑ B }}} _.
 
 Extraction Inline read_nf_impl_functional
@@ -290,21 +191,21 @@ Ltac functional_read_complete :=
       end
   end.
 
-Lemma read_nf_impl_complete : forall s d m,
-    {{ Rnf d in s ↘ m }} ->
-    exists H H', read_nf_impl s d H = exist _ m H'.
+Lemma read_nf_impl_complete {P : PtsSig} : forall i (d : domain_nf P) m,
+    {{ Rnf d in i ↘ m }} ->
+    exists H H', read_nf_impl i d H = exist _ m H'.
 Proof.
   intros; functional_read_complete.
 Qed.
 
-Lemma read_ne_impl_complete : forall s d m,
-    {{ Rne d in s ↘ m }} ->
-    exists H H', read_ne_impl s d H = exist _ m H'.
+Lemma read_ne_impl_complete {P : PtsSig} : forall i (d : domain_ne P) m,
+    {{ Rne d in i ↘ m }} ->
+    exists H H', read_ne_impl i d H = exist _ m H'.
 Proof.
   intros; functional_read_complete.
 Qed.
 
-Lemma read_typ_impl_complete : forall s d m,
+Lemma read_typ_impl_complete {P : PtsSig} : forall s (d : domain P) m,
     {{ Rtyp d in s ↘ m }} ->
     exists H H', read_typ_impl s d H = exist _ m H'.
 Proof.
